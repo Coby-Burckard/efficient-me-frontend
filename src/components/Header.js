6ed980ea9fadea1d2e9df3c8dc001015ae64af5c
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2020-03-15 14:03:37 -0500

    styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import LogInOutButton from "./LogInOutButton";
-import logo from './svg/logo.svg'
+import logo from "./svg/logo.svg";
 
 const Header = () => (
   <div className="header">
     <div className="content-container">
       <div className="header__body">
-        <img className="header__logo" src={logo} alt="logo" />
-        <LogInOutButton />
+        <div className="header__side">
+          <img className="header__logo" src={logo} alt="logo" />
+          <h1 className="header__title">Efficient Me</h1>
+        </div>
+        <div className="header__side">
+          <a
+            className="header__subtitle"
+            href="https://github.com/Coby-Burckard/efficient-me-frontend"
+          >
+            GitHub
+          </a>
+          <LogInOutButton />
+        </div>
       </div>
     </div>
   </div>
--- src/components/LogInOutButton.js
+++ src/components/LogInOutButton.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { startLogin, logout } from "../actions/user";
-import { useHistory} from 'react-router-dom'
+import { useHistory } from "react-router-dom";
 
 const LogInOutButton = () => {
   const dispatch = useDispatch();
   const user = useSelector(state => state.user.user);
-  const history = useHistory()
+  const history = useHistory();
 
   const loginClick = () => {
-    history.push("/login")
+    history.push("/login");
   };
 
   const logoutClick = () => {
@@ -19,10 +19,14 @@ const LogInOutButton = () => {
   return (
     <div>
       {!!user ? (
-        <button onClick={logoutClick}>Logout</button>
+        <button className="link-button" onClick={logoutClick}>
+          Logout
+        </button>
       ) : (
         <>
-          <button onClick={loginClick}>Login</button>
+          <button className="link-button" onClick={loginClick}>
+            Login
+          </button>
         </>
       )}
     </div>
