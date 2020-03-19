54271f4592de0a26e8793119a3bb35a127da33f6
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2020-03-19 17:31:21 -0500

    homepage styling

--- src/components/homePage/Headline.js
+++ src/components/homePage/Headline.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import headlineImg from "../svg/checklist.svg";
 
 const Headline = () => {
   const history = useHistory();
@@ -11,14 +12,19 @@ const Headline = () => {
   return (
     <div className="content-container--s">
       <div className="body-container">
-        <h1 className="header__title">Set yourself up for success</h1>
-        <h2 className="header__subtitle">
-          <span className="header__title--app-name">Efficient Me</span> allows
-          you to create and achieve SMART goals
-        </h2>
-        <button className="bold-button" onClick={handleSignUpClick}>
-          Sign Up
-        </button>
+        <div className="headline__section--text">
+          <h1 className="headline__title">Set yourself up for success</h1>
+          <h2 className="headline__subtitle">
+            <span className="headline__subtitle--app-name">Efficient Me</span>{" "}
+            allows you to create and achieve SMART goals
+          </h2>
+          <button className="bold-button" onClick={handleSignUpClick}>
+            Sign Up
+          </button>
+        </div>
+        <div className="headline__section--img">
+          <img className="headline__img" src={headlineImg} alt="checklist" />
+        </div>
       </div>
     </div>
   );
